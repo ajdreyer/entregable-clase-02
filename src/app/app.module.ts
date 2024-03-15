7fa457d661f3db58e7d4c0df7ed58b49480be065
adreyer <alejandro.dreyer@hotmail.com>
2024-03-15 20:30:19 -0300

    entregable calse 02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { StudentsPageComponent } from './students-page/students-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    StudentsPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
